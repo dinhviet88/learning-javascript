7d503d7218cec518de3d9910209bf9998dbccf5c
dinhviet88 <dinhvanviet88@gmail.com>
2023-09-18 15:45:38 +0700

    add thêm code line

--- src-javascr/java1.js
+++ src-javascr/java1.js
@@ -1,1 +1,27 @@
-console.log('xin chào các bạn')
+ console.log('xin chào các bạn')
+ let x = `hoc tap tien bo`;
+ console.log(x)
+ let a = 5;
+ let b = 15;
+ console.log(`gia tri cua 
+ bien a la : ${a} ,
+ bien b la : ${b},
+ bien a+b la : ${a+b}`)
+ let c ={
+        name:'Viet',
+        Age : 25,
+        Job : 'Worker',
+        Addr : 'Ha Noi',
+        email: 'dinhvanviet88@gmail.com'
+ };
+ console.log(
+`kieu du lieu cua c la : `, typeof c, 
+`\nkieu du lieu cua a la : `, typeof a,
+`\nkieu du lieu cua b la : `, typeof b
+ )
+console.log(`what is your name ? \nMy name is :`, c.name,`\nWhere are you from ? \nI'm from :`,c.Addr)
+console.log(`check information of C: `,c)
+//khai bao arry
+d =['MU','Chelsea','Arsenal'];
+e =[{name: 'viet',Age:35},{name:'Hong', Age:30},{name:'Huyen',Age:3}]
+console.log(e)
